099505b1c846bbc3e8f69f6998ffce02dae41371
Vladimir Jelincic <vladimir.jelincic@gmail.com>
2016-06-22 23:45:50 +0200

    Prod ready

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,5 @@
-/**
- * Created by Vladimir on 22/06/16.
- */
-import {createStore,applyMiddleware} from 'redux';
-import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-
-export default function configureStore(initialState) {
-  return createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(thunk,reduxImmutableStateInvariant())
-  );
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod');
+} else {
+  module.exports = require('./configureStore.dev');
 }
